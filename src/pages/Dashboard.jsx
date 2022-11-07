e1e949a1b5a80f0c50c9a0d2b5d73d50b977d229
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-11-07 11:40:49 +0600

    dashboard updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,65 +1,24 @@
 import React from 'react'
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from 'recharts';
+import { PieChart, Pie, Legend } from 'recharts';
 import { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
 import { getOrderSummery } from '../features/order/orderSlice'
 import { Container, Grid, Paper, Typography } from '@mui/material'
 import { HeadingPrimary } from '../shared/typography'
-import { Chart } from "react-google-charts";
 import Loader from '../component/Loader'
+import { useTheme } from '@mui/material/styles';
 function Dashboard() {
-  const data = [
-    {
-      name: 'Page A',
-      uv: 4000,
-      pv: 2400,
-      amt: 2400,
-    },
-    {
-      name: 'Page B',
-      uv: 3000,
-      pv: 1398,
-      amt: 2210,
-    },
-    {
-      name: 'Page C',
-      uv: 2000,
-      pv: 9800,
-      amt: 2290,
-    },
-    {
-      name: 'Page D',
-      uv: 2780,
-      pv: 3908,
-      amt: 2000,
-    },
-    {
-      name: 'Page E',
-      uv: 1890,
-      pv: 4800,
-      amt: 2181,
-    },
-    {
-      name: 'Page F',
-      uv: 2390,
-      pv: 3800,
-      amt: 2500,
-    },
-    {
-      name: 'Page G',
-      uv: 3490,
-      pv: 4300,
-      amt: 2100,
-    },
-  ];
+  const theme = useTheme()
   const dispatch = useDispatch()
   const { order } = useSelector((state) => state)
   useEffect(() => {
     dispatch(getOrderSummery())
   }, [dispatch])
+
   return (
     <Container>
-      <HeadingPrimary variant='h3' sx={{ color: '#333', mb:'2rem' }}>Dashboard</HeadingPrimary>
+      <HeadingPrimary variant='h3' sx={{ color: '#333', mb: '2rem' }}>Dashboard</HeadingPrimary>
       <Grid container columnSpacing={4} rowSpacing={9}>
         {order.isLoading ? <Loader /> :
           (<>
@@ -90,22 +49,50 @@ function Dashboard() {
 
             <Grid item xs={12}>
               <Typography variant='h5'>Sales</Typography>
-              <ResponsiveContainer  width='100%' height={400}>
-              <AreaChart
-                data={order.ordersSummery.dailyOrders}
-                margin={{
-                  top: 30,
-                  right: 0,
-                  left: 0,
-                  bottom: 0,
-                }}
-              >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="_id" />
-                <YAxis />
-                <Tooltip />
-                <Area type="monotone" dataKey="sales" stroke="#8884d8" fill="#f0c040" />
-              </AreaChart>
+              <ResponsiveContainer width='100%' height={400}>
+                <AreaChart
+                  data={order.ordersSummery.dailyOrders}
+                  margin={{
+                    top: 30,
+                    right: 0,
+                    left: 0,
+                    bottom: 0,
+                  }}
+                >
+                  <CartesianGrid strokeDasharray="3 3" />
+                  <XAxis dataKey="_id" />
+                  <YAxis />
+                  <Tooltip />
+                  <Area
+                    fillOpacity={1}
+                    type="monotone"
+                    dataKey="sales"
+                    stroke="#8884d8"
+                    fill={theme.palette.primary.light}
+                  />
+                </AreaChart>
+              </ResponsiveContainer>
+            </Grid>
+
+            <Grid item xs={12}>
+            <Typography variant='h5'>Category</Typography>
+              <ResponsiveContainer width="100%" height={300}>
+                <PieChart>
+                  <Pie
+                    dataKey="count"
+                    nameKey="_id"
+                    isAnimationActive={false}
+                    data={order.ordersSummery.productCategories}
+                    cx="50%"
+                    cy="50%"
+                    outerRadius={100}
+                    fill={theme.palette.primary.light}
+                    fillOpacity={1}
+                    stroke="#8884d8"
+                    label
+                  />
+                  <Tooltip />
+                </PieChart>
               </ResponsiveContainer>
             </Grid>
           </>)
