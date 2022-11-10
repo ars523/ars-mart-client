c4d10935ad0c0371212d99261cf282a154b837d4
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2022-11-10 10:51:43 +0600

    Ready for deploy

--- src/features/user/userService.js
+++ src/features/user/userService.js
@@ -1,6 +1,7 @@
 import axios from "axios"
 const URL = `/api/users`
 
+//Get all users
 export const getAllUsers = async (token)=>{
     const config = {
         headers: {
@@ -11,6 +12,7 @@ export const getAllUsers = async (token)=>{
     return res.data
 }
 
+//Get an user by id
 export const getUser = async (userId, token)=>{
     const config = {
         headers: {
@@ -21,6 +23,8 @@ export const getUser = async (userId, token)=>{
     return res.data
 }
 
+
+//Update an user by id
 export const updateUser = async (newData, token)=>{
     const config = {
         headers: {
@@ -31,7 +35,7 @@ export const updateUser = async (newData, token)=>{
     return res.data.user
 }
 
-
+//Delete an user by id
 export const deleteUser = async (userId, token)=>{
     const config = {
         headers: {
