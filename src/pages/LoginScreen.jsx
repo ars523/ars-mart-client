fc21059917b2230c49265e00b612bcb4d7f5bbee
Arifur-Rahaman <arifur.rahaman.siflu@gmail.com>
2023-05-27 07:13:20 +0600

    Update: Login page updated

--- src/pages/LoginScreen.jsx
+++ src/pages/LoginScreen.jsx
@@ -1,26 +1,26 @@
-import { Container, Grid, TextField, Typography } from "@mui/material"
+import { Container, Grid, Paper, Stack, TextField, Typography } from "@mui/material"
 import { useDispatch, useSelector } from "react-redux"
-import { useLocation, useNavigate} from "react-router-dom"
-import {signin } from "../features/auth/authSlice"
+import { useLocation, useNavigate } from "react-router-dom"
+import { signin } from "../features/auth/authSlice"
 import { ButtonPrimary } from "../shared/button"
 import { HeadingPrimary } from "../shared/typography"
 import { useFormik } from "formik"
 import * as yup from "yup"
 import { LinkPrimary } from "../shared/link"
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import Loader from "../component/Loader"
 import { useEffect } from "react"
 
 const LoginScreen = () => {
-    const {isLoading, user} = useSelector(state => state.auth)
+    const { isLoading, user } = useSelector(state => state.auth)
     const location = useLocation()
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const hitFrom = location?.state?.from
     const go = hitFrom ? hitFrom : '/'
 
-    useEffect(()=>{
-        if(user){
+    useEffect(() => {
+        if (user) {
             navigate(`${go}`)
         }
     }, [user, navigate, go])
@@ -38,74 +38,96 @@ const LoginScreen = () => {
         onSubmit: (values) => {
             const { email, password } = values
             dispatch(signin({ email, password }))
-            .unwrap()
-            .then(()=>{
-                navigate(`${go}`)
-                toast.success('Signed in successfully')
-            })
-            .catch((error)=>toast.error(error))
+                .unwrap()
+                .then(() => {
+                    navigate(`${go}`)
+                    toast.success('Signed in successfully')
+                })
+                .catch((error) => toast.error(error))
         }
     })
     const { values, handleChange, handleSubmit, errors, handleBlur, touched } = formik
 
-    if(isLoading){
-        return <Loader/>
+    if (isLoading) {
+        return <Loader />
     }
     return (
-        <Container maxWidth='sm' component='form' onSubmit={handleSubmit}>
-            <Grid container rowSpacing={3}>
-                <Grid item xs={12}>
-                    <HeadingPrimary variant="h5">
-                        Login
-                    </HeadingPrimary>
+        <Container
+            sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}
+            maxWidth='sm'
+            component='form'
+            onSubmit={handleSubmit}
+        >
+            <Paper sx={{p:'2rem'}}>
+                <Grid container rowSpacing={3}>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1" sx={{mb:'1rem'}}>Existing Users Credentials:</Typography>
+                        <Stack direction={{md:'row', sm:'column'}} spacing={'1rem'}>
+                            <Paper sx={{padding:'1rem'}} variant="outlined">
+                                <Typography align="center">User</Typography>
+                                <Typography><span style={{fontWeight:'500'}}>Email:</span> user@example.com</Typography>
+                                <Typography><span style={{fontWeight:'500'}}>Password:</span> 123456</Typography>
+                            </Paper>
+                            <Paper sx={{padding:"1rem"}} variant='outlined'>
+                                <Typography align="center">Admin</Typography>
+                                <Typography><span style={{fontWeight:'500'}}>Email:</span> admin@example.com</Typography>
+                                <Typography><span style={{fontWeight:'500'}}>Password:</span> 123456</Typography>
+                            </Paper>
+                        </Stack>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <HeadingPrimary variant="h5">
+                            Login
+                        </HeadingPrimary>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            type='email'
+                            required
+                            placeholder="Email"
+                            label="Email"
+                            name='email'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.email}
+                            error={touched.email && errors.email?.length > 0}
+                            helperText={touched.email && errors.email}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            type='password'
+                            required
+                            placeholder="Password"
+                            label="Password"
+                            name='password'
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            value={values.password}
+                            error={touched.password && errors.password?.length > 0}
+                            helperText={touched.email && errors.password}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <ButtonPrimary
+                            variant="contained"
+                            type="submit"
+                        >
+                            Sign in
+                        </ButtonPrimary>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1">
+                            Don't have an account?
+                            <LinkPrimary to='/registration' style={{ marginLeft: '1rem' }}>
+                                Register
+                            </LinkPrimary>
+                        </Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        type='email'
-                        required
-                        placeholder="Email"
-                        label="Email"
-                        name='email'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.email}
-                        error={touched.email && errors.email?.length > 0}
-                        helperText={touched.email&&errors.email}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        type='password'
-                        required
-                        placeholder="Password"
-                        label="Password"
-                        name='password'
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        value={values.password}
-                        error={touched.password && errors.password?.length > 0}
-                        helperText={touched.email&&errors.password}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <ButtonPrimary
-                        variant="contained"
-                        type="submit"
-                    >
-                        Sign in
-                    </ButtonPrimary>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">
-                        Don't have an account?
-                        <LinkPrimary to='/registration' style={{ marginLeft: '1rem' }}>
-                            Register
-                        </LinkPrimary>
-                    </Typography>
-                </Grid>
-            </Grid>
+            </Paper>
         </Container>
     );
 };
--- src/pages/RegisterScreen.jsx
+++ src/pages/RegisterScreen.jsx
@@ -1,26 +1,26 @@
 import React, { useEffect } from 'react';
-import { Button, Grid, TextField, Typography } from '@mui/material';
+import { Button, Grid, Paper, TextField, Typography } from '@mui/material';
 import { Container } from '@mui/system';
 import { useFormik } from 'formik';
 import * as yup from 'yup'
 import { HeadingPrimary } from '../shared/typography';
 import { LinkPrimary } from '../shared/link';
-import { signup} from '../features/auth/authSlice';
+import { signup } from '../features/auth/authSlice';
 import { useDispatch, useSelector } from 'react-redux'
 import { useLocation, useNavigate } from 'react-router-dom';
-import {toast} from 'react-toastify';
+import { toast } from 'react-toastify';
 import Loader from "../component/Loader"
 
 const RegisterScreen = () => {
     const dispatch = useDispatch()
-    const {isLoading, user} = useSelector(state => state.auth)
+    const { isLoading, user } = useSelector(state => state.auth)
     const location = useLocation()
     const navigate = useNavigate()
     const hitFrom = location?.state?.from
     const go = hitFrom ? hitFrom : '/'
 
-    useEffect(()=>{
-        if(user){
+    useEffect(() => {
+        if (user) {
             navigate(`${go}`)
         }
     }, [user, navigate, go])
@@ -54,83 +54,89 @@ const RegisterScreen = () => {
     const { values, handleChange, handleSubmit, errors, handleBlur, touched } = formik
     const { name, email, password, confirmPassword } = values
 
-    if(isLoading){
-        <Loader/>
+    if (isLoading) {
+        <Loader />
     }
     return (
-        <Container maxWidth='sm' sx={{ p: '30px' }}>
-            <Grid container rowSpacing={2} component='form' onSubmit={handleSubmit}>
-                <Grid item xs={12}>
-                    <HeadingPrimary variant="h5">
-                        Sign Up
-                    </HeadingPrimary>
+        <Container
+            maxWidth='sm'
+            sx={{ height: '100vh', display: 'flex', alignItems: 'center' }}
+        >
+            <Paper sx={{p:'2rem'}}>
+                <Grid container rowSpacing={2} component='form' onSubmit={handleSubmit}>
+                    <Grid item xs={12}>
+                        <HeadingPrimary variant="h5">
+                            Sign Up
+                        </HeadingPrimary>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            placeholder='Name'
+                            label='Name'
+                            name='name'
+                            value={name}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.name && errors.name?.length > 0}
+                            helperText={touched.email && errors.name}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            placeholder='Email'
+                            label='Email'
+                            name='email'
+                            value={email}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.email && errors.email?.length > 0}
+                            helperText={touched.email && errors.email}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            placeholder='Password'
+                            label='Password'
+                            type='password'
+                            name='password'
+                            value={password}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.password && errors.password?.length > 0}
+                            helperText={touched.email && errors.password}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <TextField
+                            fullWidth
+                            placeholder='Confirm Password'
+                            label='Confirm Password'
+                            type='password'
+                            name='confirmPassword'
+                            value={confirmPassword}
+                            onChange={handleChange}
+                            onBlur={handleBlur}
+                            error={touched.confirmPassword && errors.confirmPassword?.length > 0}
+                            helperText={touched.email && errors.confirmPassword}
+                        />
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Button variant='contained' type='submit'>Submit</Button>
+                    </Grid>
+                    <Grid item xs={12}>
+                        <Typography variant="subtitle1">
+                            Have an account?
+                            <LinkPrimary to='/login' style={{ marginLeft: '1rem' }}>
+                                Sign in
+                            </LinkPrimary>
+                        </Typography>
+                    </Grid>
                 </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        placeholder='Name'
-                        label='Name'
-                        name='name'
-                        value={name}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.name && errors.name?.length > 0}
-                        helperText={touched.email && errors.name}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        placeholder='Email'
-                        label='Email'
-                        name='email'
-                        value={email}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.email && errors.email?.length > 0}
-                        helperText={touched.email && errors.email}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        placeholder='Password'
-                        label='Password'
-                        type='password'
-                        name='password'
-                        value={password}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.password && errors.password?.length > 0}
-                        helperText={touched.email && errors.password}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <TextField
-                        fullWidth
-                        placeholder='Confirm Password'
-                        label='Confirm Password'
-                        type='password'
-                        name='confirmPassword'
-                        value={confirmPassword}
-                        onChange={handleChange}
-                        onBlur={handleBlur}
-                        error={touched.confirmPassword && errors.confirmPassword?.length > 0}
-                        helperText={touched.email && errors.confirmPassword}
-                    />
-                </Grid>
-                <Grid item xs={12}>
-                    <Button variant='contained' type='submit'>Submit</Button>
-                </Grid>
-                <Grid item xs={12}>
-                    <Typography variant="subtitle1">
-                        Have an account?
-                        <LinkPrimary to='/login' style={{ marginLeft: '1rem' }}>
-                            Sign in
-                        </LinkPrimary>
-                    </Typography>
-                </Grid>
-            </Grid>
+            </Paper>
+
         </Container>
     );
 };
